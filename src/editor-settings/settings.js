c993d9d3d428ab911efec9869ac17688d67f0161
jwklong <jwklong@gmx.com>
2026-09-19 18:44:17 +0100

    pen pressure setting
    Signed-off-by: jwklong <jwklong@gmx.com>

--- src/editor-settings/settings.js
+++ src/editor-settings/settings.js
@@ -12,6 +12,7 @@ export default Object.fromEntries([
     'blockPinning',
     'categoryReordering',
     'paintMultiTool',
+    'paintPenPressure',
     'paintScrollZoom',
     'soundDisplayDetail',
     'showExtensionIds',

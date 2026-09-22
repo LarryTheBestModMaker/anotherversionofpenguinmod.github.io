3a9d1948691d4e6ed337ba3bdca78d884661ae9d
RedMan13 <godslayerakp@gmail.com>
2026-09-22 11:27:31 -0700

    add open paint option to the splash screen

--- src/containers/splash-modal.jsx
+++ src/containers/splash-modal.jsx
@@ -3,6 +3,9 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import SplashModalComponent from '../components/splash-modal/splash-modal.jsx';
 import {closeSplashModal} from '../reducers/modals';
+import {activateTab, COSTUMES_TAB_INDEX} from '../reducers/editor-tab';
+import {STAGE_DISPLAY_SCALE_METADATA, STAGE_DISPLAY_SIZES, STAGE_SIZE_MODES} from '../lib/layout-constants';
+import {setStageSize} from '../reducers/stage-size';
 import VM from 'scratch-vm';
 
 const SplashModal = props => (
@@ -19,7 +22,12 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-    onClose: () => dispatch(closeSplashModal())
+    onClose: () => dispatch(closeSplashModal()),
+    onOpenPaint: () => {
+        dispatch(closeSplashModal());
+        dispatch(activateTab(COSTUMES_TAB_INDEX));
+        dispatch(setStageSize(STAGE_SIZE_MODES.small))
+    }
 });
 
 export default connect(
